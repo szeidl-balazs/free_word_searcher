967c313078a7b9d429076dcda46914c7ab876eee
Balázs Szeidl <szeidl.balazs@hotmail.com>
2021-03-23 23:00:45 +0100

    builded, dockerfile prepared

---
+++ class-to-function/build/precache-manifest.8ae0a29a70f9dfff23705897b826c6a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e23af5c2989340d63fb46eb8ac75a2f1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1bf9b98c3348344fb8fe",
+    "url": "./static/css/main.1cd8d0b3.chunk.css"
+  },
+  {
+    "revision": "58e765996b024907f654",
+    "url": "./static/js/2.ec0285b2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ec0285b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf9b98c3348344fb8fe",
+    "url": "./static/js/main.bb795885.chunk.js"
+  },
+  {
+    "revision": "591fa9b0bbe802165a99",
+    "url": "./static/js/runtime-main.7fc33d63.js"
+  }
+]);
